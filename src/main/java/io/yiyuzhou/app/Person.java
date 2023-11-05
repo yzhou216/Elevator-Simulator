446fed6e906d51cbc5e35fc23f07624d25be2453
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-11-05 01:24:49 -0700

    Add random Person constructor

--- src/main/java/io/yiyuzhou/app/Person.java
+++ src/main/java/io/yiyuzhou/app/Person.java
@@ -1,11 +1,19 @@
 package io.yiyuzhou.app;
 
+import java.util.Random;
+
 class Person implements Comparable<Person>{
 	private int start;
 	private int dest;
+	private static final Random random = new Random();
 
-	public Person(int maxFloor) {
-		/* TODO: random start and dest */
+	/* creates a Person object has the dest of a random floor */
+	public Person(int start, int maxFloor) {
+		this.start = start;
+
+		do {
+		    this.dest = random.nextInt(maxFloor) + 1;
+		} while (this.dest == this.start); /* person can't start and go to the same floor */
 	}
 
 	public int compareTo(Person person) {
