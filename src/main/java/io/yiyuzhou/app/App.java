e17fa9571c77263dd5ab6e04de3988731051aedf
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-11-05 15:25:18 -0800

    Add tick

--- src/main/java/io/yiyuzhou/app/App.java
+++ src/main/java/io/yiyuzhou/app/App.java
@@ -1,13 +1,91 @@
 package io.yiyuzhou.app;
 
 public class App {
+	private static void tick(Elevator[] elevators, Floor[] floors, int numOfFloors, float passengersProbablity) {
+		/* random generate passengers on different floors */
+		for (int i = 0; i < floors.length; i++) {
+			if (Math.random() < passengersProbablity)
+				floors[i].addPerson(new Person(floors[i].floorNum, numOfFloors - 1));
+		}
+
+		for (int i = 0; i < elevators.length; i++) {
+			/* send elevator down or up if it's on the top floor or the bottom floor */
+			if (elevators[i].getCurFloor() == numOfFloors - 1) {
+				if (elevators[i].isGoingUp())
+					elevators[i].setGoingUp(false);
+				else
+					elevators[i].setGoingUp(true);
+			} else if (elevators[i].getCurFloor() == 0) {
+				if (!elevators[i].isGoingUp())
+					elevators[i].setGoingUp(true);
+				else
+					elevators[i].setGoingUp(true);
+			}
+
+			if (elevators[i].isGoingUp()) {
+				/* case when the elevator is empty */
+				//Person root = elevators[i].getMinHeap().peek();
+				if (elevators[i].getMinHeap().isEmpty()) {
+					if (elevators[i].getCurFloor() + 5 <= numOfFloors - 1) {
+						elevators[i].setCurFloor(elevators[i].getCurFloor() + 5);
+					} else {
+						elevators[i].setCurFloor(numOfFloors - 1);
+					}
+				} else {
+					/* TODO: elevator can move no more than 5 floors each time */
+					elevators[i].setCurFloor(elevators[i].getMinHeap().peek().getDest());
+				}
+
+			} else if (!elevators[i].isGoingUp()) {
+				if (elevators[i].getMaxHeap().isEmpty()) {
+					if (elevators[i].getCurFloor() - 5 >= 0) {
+						elevators[i].setCurFloor(elevators[i].getCurFloor() - 5);
+					} else {
+						elevators[i].setCurFloor(0);
+					}
+				} else {
+					elevators[i].setCurFloor(elevators[i].getMaxHeap().peek().getDest());
+				}
+			}
+
+			/* debug */
+			if (elevators[i].isGoingUp())
+				System.out.println("going up");
+			else
+				System.out.println("going down");
+			System.out.printf("num of min heap passengers: %d\n", elevators[i].getMinHeap().size());
+			System.out.printf("num of max heap passengers: %d\n", elevators[i].getMaxHeap().size());
+			System.out.printf("current floor: %d\n\n", elevators[i].getCurFloor()); /* debug */
+
+			elevators[i].unload(elevators[i].getCurFloor());
+
+			if (elevators[i].isGoingUp()) {
+				elevators[i].load(floors[elevators[i].getCurFloor()].getPassengersUp());
+			} else {
+				elevators[i].load(floors[elevators[i].getCurFloor()].getPassengersDown());
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		Env env = new Env(args[0]);
-		System.out.println(env.getProperty("structures"));
-		System.out.println(env.getProperty("floors"));
-		System.out.println(env.getProperty("passengers"));
-		System.out.println(env.getProperty("elevators"));
-		System.out.println(env.getProperty("elevatorCapacity"));
-		System.out.println(env.getProperty("duration"));
+		final String structure = env.getProperty("structures");
+		final int numOfFloors = Integer.parseInt(env.getProperty("floors"));
+		final float passengersProbability = Float.parseFloat(env.getProperty("passengers"));
+		final int numOfElevators = Integer.parseInt(env.getProperty("elevators"));
+		final int elevatorCapacity = Integer.parseInt(env.getProperty("elevatorCapacity"));
+		final int duration = Integer.parseInt(env.getProperty("duration"));
+
+		Elevator[] elevators = new Elevator[numOfElevators];
+		for (int i = 0; i < numOfElevators; i++)
+			elevators[i] = new Elevator(elevatorCapacity);
+
+		Floor[] floors = new Floor[numOfFloors];
+		for (int i = 0; i < numOfFloors; i++)
+			floors[i] = new Floor(i, structure);
+
+		for (int i = 0; i < duration; i++) {
+			tick(elevators, floors, numOfFloors, passengersProbability);
+		}
 	}
 }
